5bccc57fdba0c82258b6b2b18b3442ea77d852cb
BasakCanbaz <basakcanbazz@gmail.com>
2022-08-02 23:36:31 +0300

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import classes from "./Header.module.css"
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton onClick={props.onShowCart}/>
+        </header>
+        <div className={classes["main-image"]}>
+            <img src="https://github.com/academind/react-complete-guide-code/blob/11-practice-food-order-app/extra-files/meals.jpg?raw=true" 
+            alt="A table full of delicious food!"/>
+        </div>
+    </Fragment>
+   );
+};
+
+export default Header;
